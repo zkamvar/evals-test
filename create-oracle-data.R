4577c3183f2863ced33a175bf668c2debd2da574
Evan L. Ray <elray@umass.edu>
2025-01-09 13:24:32 -0500

    add the R script

---
+++ create-oracle-data.R
@@ -0,0 +1,6 @@
+target_data <- readr::read_csv("https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/refs/heads/main/target-data/target-hospital-admissions.csv")
+oracle_output <- target_data |>
+  dplyr::select(-weekly_rate, -location_name) |>
+  dplyr::rename(target_end_date = date, oracle_value = value)
+
+write.csv(oracle_output, "oracle-output.csv", row.names = FALSE)
