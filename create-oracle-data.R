a48130f1f2069c9a1b0a8cbaed289f55aebebae4
Evan L. Ray <elray@umass.edu>
2025-01-10 16:06:44 -0500

    add horizon to oracle output, this is a hack

--- create-oracle-data.R
+++ create-oracle-data.R
@@ -1,6 +1,7 @@
 target_data <- readr::read_csv("https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/refs/heads/main/target-data/target-hospital-admissions.csv")
 oracle_output <- target_data |>
   dplyr::select(-weekly_rate, -location_name) |>
-  dplyr::rename(target_end_date = date, oracle_value = value)
+  dplyr::rename(target_end_date = date, oracle_value = value) |>
+  dplyr::cross_join(data.frame(horizon = 0:3))
 
 write.csv(oracle_output, "oracle-output.csv", row.names = FALSE)
