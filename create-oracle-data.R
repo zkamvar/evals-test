c234e1dcca93a7cf7faf39c2a262ea5b2401ef3a
Evan L. Ray <elray@umass.edu>
2025-01-13 09:22:26 -0500

    remove us from oracle output

--- create-oracle-data.R
+++ create-oracle-data.R
@@ -1,5 +1,6 @@
 target_data <- readr::read_csv("https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/refs/heads/main/target-data/target-hospital-admissions.csv")
 oracle_output <- target_data |>
+  dplyr::filter(location != "US") |>
   dplyr::select(-weekly_rate, -location_name) |>
   dplyr::rename(target_end_date = date, oracle_value = value) |>
   dplyr::cross_join(data.frame(horizon = 0:3))
